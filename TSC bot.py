dc85e9f44e01c4e3af23704a9d0a991e5375823d
StarlordDev <75289436+starlordXD69@users.noreply.github.com>
2022-05-14 14:47:42 -0500

    Updated TSC to match the singular lib D.py2.0

--- TSC bot.py
+++ TSC bot.py
@@ -1,13 +1,13 @@
 import discord
-from discord.ext import commands,tasks
-from discord.commands import Option
+from discord.ext import commands, tasks
+from discord import app_commands
 import pymongo
 import asyncio
 import random
 intents = discord.Intents.all()
-bot = commands.Bot(command_prefix="!",intents = intents,status=discord.Status.dnd,activity=discord.Activity(name='Maintenance',type=discord.ActivityType.watching))
+bot = commands.Bot(command_prefix="!",intents = intents,status=discord.Status.dnd,activity=discord.Activity(name='TSC',type=discord.ActivityType.watching))
 
-x = pymongo.MongoClient("DATABASE STRING")
+x = pymongo.MongoClient("DB connection string")
 LB = x.Leaderboard
 lb_users = LB.User
 lb_teams = LB.Team
@@ -37,9 +37,9 @@ def assign_team(id):
         Vexusc = Vexusc + 1
     for i in Exos:
         Exosc = Exosc + 1
-    if Aetasc >= Azurec: team = 'Azure'
-    if Azurec >= Vexusc: team = 'Vexus'
-    if Vexusc >= Exosc: team = 'Exos'
+    if Aetasc > Azurec: team = 'Azure'
+    if Azurec > Vexusc: team = 'Vexus'
+    if Vexusc > Exosc: team = 'Exos'
     Users.insert_one({'user':id,'team':team})
     return team
 
@@ -69,15 +69,26 @@ async def give_points(message,id):
 async def on_ready():
     print(f'{bot.user} is online.')
 
-@bot.slash_command( guild_ids = [877460893439512627])
+
+@bot.tree.command()
 async def ping(ctx):
     """ping pong"""
-    await ctx.respond(f"pong: {bot.latency}")
+    await ctx.response.send_message(f"pong: {bot.latency}")
 
-@bot.slash_command( guild_ids = [877460893439512627])
-async def leaderboard(ctx,type:Option(str,"Type of leaderboard",required=True,choices=["Player","Team"])):
+@bot.tree.command()
+async def repo(ctx:discord.Interaction):
+    """The Repository for this bot"""
+    await ctx.response.send_message("https://github.com/starlordXD69/TheStudyCornerBot")
+types= []
+Player = app_commands.Choice(name='Player',value='Player')
+team_option = app_commands.Choice(name='Team',value='Team')
+types.append(Player)
+types.append(team_option)
+@bot.tree.command()
+@app_commands.choices(type=types)
+async def leaderboard(ctx:discord.Interaction,type:str):
     """The current leaderboard"""
-    await ctx.defer()
+    await ctx.response.defer()
     if type == "Player":
         user = Users.find()
         user = user.sort('points', -1)
@@ -89,74 +100,58 @@ async def leaderboard(ctx,type:Option(str,"Type of leaderboard",required=True,ch
             user = bot.get_user(i.get('user'))
             embed.add_field(name=user.name,value=f'{i.get("points")} points!',inline=False)
             count = count + 1
-        await ctx.respond(embed=embed)
+        await ctx.followup.send(embed=embed)
     else:
         embed = discord.Embed(title='Leaderboard for Teams',color=0xee9ad9)
         team = lb_teams.find()
         team = team.sort('points',-1)
         for info in team:
             embed.add_field(name=info.get('team'),value=f"{info.get('points')} points!",inline=False)
-        await ctx.respond(embed=embed)
+        await ctx.followup.send(embed=embed)
 
-@bot.slash_command(guild_ids = [877460893439512627])
+@bot.tree.command(guild=discord.Object(630014101887778816))
 @commands.has_permissions(administrator=True)
-async def reset(ctx,type:Option(str,"Which type to reset",required=True,choices=["Player","Team"])):
+@app_commands.choices(type=types)
+async def reset(ctx:discord.Interaction,type:str):
     """Reset the entirety of the leaderboard || NOT SPECIFIC PLAYERS"""
-    await ctx.defer()
+    await ctx.response.defer()
     if type == 'Player':
         u = Users.find()
         try:
             for users in u:
                 Users.delete_one({"user":users.get('user')})
-            await ctx.respond("Successfully deleting.")
+            await ctx.followup.send("Successfully deleting.")
         except Exception as e:
-            await ctx.respond(f'Error: {e}')
+            await ctx.followup.send(f'Error: {e}')
     else:
         team_info = lb_teams.find()
         try:
             for info in team_info:
                 lb_teams.update_one({'team':info.get('team')},{"$set":{"points":0}})
-            await ctx.respond("Successfully deleted.")
+            await ctx.followup.send("Successfully deleted.")
         except Exception as e:
-            await ctx.respond(f'Error: {e}')
+            await ctx.followup.send(f'Error: {e}')
 
-@reset.error
-async def reset_error(ctx,error):
-    if isinstance(error,commands.MissingPermissions):
-        await ctx.respond("Invalid Permissions")
-    else:
-        ctx.respond(error)
 
-@bot.slash_command(guild_ids = [877460893439512627])
+@bot.tree.command(guild=discord.Object(630014101887778816))
 @commands.has_permissions(administrator=True)
-async def playerreset(ctx,user:discord.User):
+async def playerreset(ctx:discord.Interaction,user:discord.User):
     """Specific player points reset"""
-    await ctx.defer()
+    await ctx.response.defer()
     try:
         Users.delete_one({'user':user.id})
-        await ctx.respond("Successfully deleted.")
+        await ctx.followup.send("Successfully deleted.")
     except Exception as e:
-        await ctx.respond(f"Error: {e}")
-
-@playerreset.error
-async def playerreset_error(ctx,error):
-    if isinstance(error,commands.MissingPermissions):
-        await ctx.respond("Invalid Permissions")
-    else:
-        ctx.respond(error)
-
-
-
+        await ctx.followup.send(f"Error: {e}")
 question_info = {}
-@tasks.loop(minutes=5)
+@tasks.loop(hours=1)
 async def question_sender():
     global question_info
     await bot.wait_until_ready()
     amount = que.find_one({'question':"STARLORD"})
     amount = amount.get('amount')
     question_num = random.randint(1,amount)
-    #channel =  bot.get_channel(955453693866696734)
-    channel = bot.get_channel(926681570973196388)
+    channel = bot.get_channel(931869372425830410)
     _question_info = que.find_one({'custom_id':question_num})
     question = _question_info.get("question")
     answer = _question_info.get("answer").lower()
@@ -165,30 +160,64 @@ async def question_sender():
     embed.set_author(name="The Study Corner", icon_url=bot.user.avatar.url)
     embed.set_footer(text=f'category: {_question_info.get("category")} || questionID: {_question_info.get("custom_id")} ')
     send = await channel.send(embed=embed)
-    question_info = {'answer':answer,'embed':send}
+    try:
+        await send.pin()
+    except:
+        pass
+    question_info = {'answer':answer,'embed':send,'channel':channel.id}
+    question_checker.start()
+@tasks.loop(minutes=31)
+async def question_checker():
+    global question_info
     await asyncio.sleep(1800)
     if question_info != None:
+        channel = bot.get_channel(question_info.get('channel'))
+        sendt = question_info.get('embed')
         await channel.send('No one got the answer. Too bad :/')
+        try:
+            await sendt.unpin()
+        except:
+            pass
         question_info = None
-    else:
-        pass
-
-
-
+        question_checker.stop()
 
 @bot.event
 async def on_message(message):
     global question_info
-    if message.content and message.channel.id == 926681570973196388:
-        #channel = bot.get_channel(955453693866696734)
-        channel = bot.get_channel(926681570973196388)
-        message.content = message.content.lower()
-        if message.content == question_info.get('answer'):
-            question_info = {}
-            team = await give_points(message,message.author.id)
-            await channel.send(f"{message.author.mention} got the answer. 5 points for team {team}")
+    if message.content and message.channel.id == 931869372425830410:
+        if message.author.id == bot.user.id:
+            return
+        else:
+            channel = bot.get_channel(931869372425830410)
+            message.content = message.content.lower()
+            if message.content == question_info.get('answer'):
+                embed = question_info.get("embed")
+                try:
+                    await embed.unpin()
+                    await embed.delete()
+                    await message.delete()
+                except:
+                    pass
+                question_info = {}
+                team = await give_points(message,message.author.id)
+                await channel.send(f"<@{message.author.id}> got the answer. 5 points for team {team}")
 
 
+#Maintenence comannds
+@bot.command()
+@commands.is_owner()
+async def syncTM(ctx):
+    if ctx.author.id == 705992469426339841:
+        await bot.tree.sync(guild=ctx.guild)
+        await ctx.send("Successfully Synced")
+
+
+@bot.command()
+@commands.is_owner()
+async def syncall(ctx):
+    if ctx.author.id == 705992469426339841:
+        await bot.tree.sync()
+        await ctx.send("Successfully Synced to all guilds")
 
 question_sender.start()
-bot.run("BOT TOKEN")
+bot.run("")
